a2d8b1e9cee9f0e8b05bc73804ce3670fab873c4
Alexey Bratushka <seomasterweb@gmail.com>
2019-02-26 13:28:40 +0200

    Reverting repo url underscore style as nest-cli will now allow underscores in project name

--- generators/app/index.js
+++ generators/app/index.js
@@ -57,7 +57,7 @@ class Agonb extends Generator {
 
       projectDestinationPath = projectDestinationPath.replace(/\.git$/, '');
 
-      [, projectApplicationName] = projectDestinationPath.match(/^(.+)_backend_app$/)
+      [, projectApplicationName] = projectDestinationPath.match(/^(.+)-backend-app$/)
     } catch (e) {
       throw new Error('Error during application directory/name parsing')
     }
