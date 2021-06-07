09b53cf9fdf6f0a0663cdfb57b918428b8894511
Andriy Yakovenko <andriy.yakovenko@brocoders.team>
2021-06-07 15:32:28 +0300

    remove iam user usage

--- generators/nestjs-scripts/templates/scripts/aws.sh
+++ generators/nestjs-scripts/templates/scripts/aws.sh
@@ -23,16 +23,12 @@ init_aws_env() {
     db_host=$(get_ssm_param "db/host")
     s3_name=$(get_ssm_param "s3/name")
     s3_base_url=$(get_ssm_param "s3/domain")
-    user_access_key_id=$(get_ssm_param "iam/access-key-id")
-    user_secret_access_key=$(get_ssm_param "iam/secret-access-key")
 
     # also you can export env variables over write to file and read from file
     export DATABASE_NAME=$db_name
     export DATABASE_USERNAME=$db_user
     export DATABASE_PASSWORD=$db_password
     export DATABASE_HOST=$db_host
-    export ACCESS_KEY_ID=$user_access_key_id
-    export SECRET_ACCESS_KEY=$user_secret_access_key
     export AWS_DEFAULT_S3_BUCKET=$s3_name
     export AWS_DEFAULT_S3_URL=$s3_base_url
     export AWS_S3_REGION=$env_region
